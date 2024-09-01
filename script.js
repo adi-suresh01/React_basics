aaa3984f07426333fbcd412ce30649228f98653d
adi-suresh01 <aditya.suresh22@gmail.com>
2024-08-31 22:28:14 -0400

    eventlistener

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('pokemon is cool');
+var string="dbz";
+var num = 21;
+
+function printNums(){
+    for (i=0; i<=num; i++){
+        console.log(i);
+    }
+}
+
+printNums();
+
+document.getElementById('box').addEventListener('click', function(){
+    alert('pokemon added');
+});
